12b7f56d200a528d88db29b2a8665227b01fc009
Alexander Gantchev <Alexnader.Gantchev@studentf.ch>
2024-08-29 11:34:03 +0200

    Typedebase

---
+++ exercices/exercice03/Typedebase.java
@@ -0,0 +1,15 @@
+package exercices.exercice03;
+
+public class Typedebase {
+    public static void main(String[] args) {
+        int age = 16;
+        int nbr = 10 ;
+        int somme = age + nbr;
+        String ch2 = "alexander";
+
+        System.out.println("Je m'appelle" + ch2 + "et j'ai" + age + "ans.");
+        System.out.println("dans 10 ans, j'aurai" + somme  );
+
+    }
+
+}
