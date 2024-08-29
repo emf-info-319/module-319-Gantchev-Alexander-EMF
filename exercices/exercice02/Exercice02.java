ad47465b7aedde42a207f4b1efee031fe8c69b31
Alexander Gantchev <Alexnader.Gantchev@studentf.ch>
2024-08-29 10:37:52 +0200

    Exercice02

---
+++ exercices/exercice02/Exercice02.java
@@ -0,0 +1,10 @@
+package exercices.exercice02;
+
+public class Exercice02 {
+    public static void main(String[] args) {
+        System.out.println("Bonjour tout le monde!");
+        System.out.println("Coucou toi !");
+        System.out.println("Incroyable");
+    }
+
+}
