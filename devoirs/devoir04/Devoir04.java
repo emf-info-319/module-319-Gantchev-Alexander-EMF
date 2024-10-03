8cfe5ebb1a43aff4c87172e6d5c6ba771be669bb
Alexander Gantchev <Alexnader.Gantchev@studentf.ch>
2024-10-03 11:43:05 +0200

    Voila pour l'instant :(

---
+++ devoirs/devoir04/Devoir04.java
@@ -0,0 +1,14 @@
+package devoirs.devoir04;
+
+public class Devoir04 {
+    public static void main(String[] args) {
+        int year = 2024;
+        int month = 10;
+        int day = 3;
+
+        if (year>9999) {
+            System.out.println("l'année [ "+ year+);
+        }
+    }
+
+}
