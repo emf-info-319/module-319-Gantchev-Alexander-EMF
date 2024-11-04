8be23ff92d6779bddad6c863699c7a50ba6ec5dc
John Doe <Alexander.Gantchev@studentfr.ch>
2024-11-04 19:21:41 +0100

    bon

--- exercices/exercice10/ExercicesMéthodesTableaux.java
+++ exercices/exercice10/ExercicesMéthodesTableaux.java
@@ -39,26 +39,91 @@ public class ExercicesMéthodesTableaux {
         return tab.length;
 
     }
+
     public static void afficherTableau(int[] tableau) {
         for (int i = 0; i < tableau.length; i++) {
-            
+
         }
 
+    }
 
+    public static int trouverMinimum(int[] tableau) {
+        int minimum = tableau[0];
 
-    }public static int trouverMinimum(int[] tableau) {
-        int minimum = tableau[0]; 
-        
-                for (int i = 1; i < tableau.length; i++) {
-                    
-                    if (tableau[i] < minimum) {
-                        System.out.println(i);
-                        minimum = tableau[i];
-                    }
+        for (int i = 1; i < tableau.length; i++) {
 
-                }
-                System.out.println(minimum);
-                return minimum;
+            if (tableau[i] < minimum) {
+                System.out.println(i);
+                minimum = tableau[i];
             }
+
+        }
+        System.out.println(minimum);
+        return minimum;
+    }
+
+    public static int trouverMaximum(int[] tableau) {
+        int maximum = tableau[0];
+
+        for (int i = 1; i < tableau.length; i++) {
+            if (tableau[i] > maximum) {
+                maximum = tableau[i];
+            }
+        }
+        return maximum;
+    }public static int compterFrequence(int[] tableau, int valeur) {
+        int frequence = 0;
+
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i] == valeur) {
+                frequence++;
+            }
+        }
+        return frequence;
+    }public static int calculerSomme(int[] tableau) {
+        int somme = 0;
+
+        for (int i = 0; i < tableau.length; i++) {
+            somme += tableau[i];
+        }
+        return somme;
+    }public static double calculerMoyenne(int[] tableau) {
         
+
+        double somme = 0;
+
+        for (int i = 0; i < tableau.length; i++) {
+            somme += tableau[i];
+        }	
+
+        return somme / tableau.length;
+    }
+    public static void remplacerValeur(int[] tableau, int valeurCible, int nouvelleValeur) {
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i] == valeurCible) {
+                tableau[i] = nouvelleValeur;
+            }
+        }
+    }public static int trouverPremiereOccurrence(int[] tableau, int valeurRecherchee) {
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i] == valeurRecherchee) {
+                return i; // Retourne l'indice de la première occurrence trouvée
+            }
+        }
+        return -1; // Retourne -1 si la valeur n'est pas trouvée dans le tableau
+    }public static int trouverDerniereOccurrence(int[] tableau, int valeurRecherchee) {
+        for (int i = tableau.length - 1; i >= 0; i--) {
+            if (tableau[i] == valeurRecherchee) {
+                return i; // Retourne l'indice de la dernière occurrence trouvée
+            }
+        }
+        return -1; // Retourne -1 si la valeur n'est pas trouvée dans le tableau
+    }
+
+
+
+
+
+
+
 }
